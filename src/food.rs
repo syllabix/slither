cf8b66b3c4f3c4215b4b8f0dd537b099e179a97d
Tom Stoepker <tom.stoepker@gmail.com>
2025-02-08 16:34:38 +0100

    trying to get some timer action

--- src/food.rs
+++ src/food.rs
@@ -25,20 +25,27 @@ use crate::arena::{self, Position, Size};
 
 const FOOD_COLOR: Color = Color::srgb(1.0, 0.0, 1.0);
 
+#[derive(Resource)]
+pub struct FoodTimer {
+    clock: Timer
+}
+
 /// Component that marks an entity as collectible food
 #[derive(Component)]
 pub struct Food;
 
 /// Spawns initial food and respawns food when collected
-pub fn spawn(mut commands: Commands) {
-    let x = (random::<f32>() * arena::WIDTH) as i32;
-    let y = (random::<f32>() * arena::HEIGHT) as i32;
-    commands.spawn(Sprite {
-        color: FOOD_COLOR,
-        ..Default::default()
-    })
-    .insert(Food)
-    .insert(Position { x, y })
-    .insert(Size::square(0.8));
+pub fn spawn(time: Res<Time>, mut timer: ResMut<FoodTimer>, mut commands: Commands) {
+    if timer.clock.tick(time.delta()).just_finished() {
+        let x = (random::<f32>() * arena::WIDTH) as i32;
+        let y = (random::<f32>() * arena::HEIGHT) as i32;
+        commands.spawn(Sprite {
+            color: FOOD_COLOR,
+            ..Default::default()
+        })
+        .insert(Food)
+        .insert(Position { x, y })
+        .insert(Size::square(0.8));
+    }
 }
 
