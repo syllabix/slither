c87edfa7d8c484d459b7e5798498430593db76ef
Tom Stoepker <tom.stoepker@gmail.com>
2025-02-08 16:18:30 +0100

    task: bootstrap basic snake and areana systems and components

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    window::{Window, WindowPlugin},
+};
+
+mod snake;
+mod arena;
 
 fn main() {
     App::new()
-    .add_systems(Startup, setup_camera)
-    .add_plugins(DefaultPlugins).run();
+        .insert_resource(ClearColor(Color::srgb(0.04, 0.04, 0.04))) // Dark gray background
+        .add_systems(Startup, (setup_camera, snake::spawn).chain())
+        .add_systems(Update, snake::movement)
+        .add_systems(PostUpdate, (arena::position_translation, arena::scale_size))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Snake Game".into(),
+                resolution: (500., 500.).into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .run();
 }
 
 fn setup_camera(mut commands: Commands) {
