6d40051b573776edf22cc97ba3b5140b3180d222
Tom Stoepker <tom.stoepker@gmail.com>
2025-02-08 17:49:50 +0100

    task: plot the tail

--- src/snake.rs
+++ src/snake.rs
@@ -9,12 +9,14 @@
 //! - Movement and growth systems
 //! - Collision detection with food and self
 
+use std::slice::Iter;
 
 use bevy::{
     app::{FixedUpdate, Plugin, Startup},
     color::Color,
     ecs::{
         component::Component,
+        entity::Entity,
         schedule::IntoSystemConfigs,
         system::{Commands, Query, Res, ResMut, Resource},
     },
@@ -45,6 +47,7 @@ impl Direction {
 }
 
 const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
+const SNAKE_SEGMENT_COLOR: Color = Color::srgb(0.3, 0.3, 0.3);
 
 #[derive(Component)]
 struct SnakeHead {
@@ -59,15 +62,47 @@ impl Default for SnakeHead {
     }
 }
 
-fn spawn(mut commands: Commands) {
+#[derive(Component)]
+struct SnakeSegment;
+
+#[derive(Resource, Default)]
+struct SnakeSegments(Vec<Entity>);
+
+impl SnakeSegments {
+    fn iter(&self) -> Iter<Entity> {
+        self.0.iter()
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+fn spawn_segment(mut commands: Commands, position: Position) -> Entity {
     commands
         .spawn(Sprite {
-            color: SNAKE_HEAD_COLOR,
+            color: SNAKE_SEGMENT_COLOR,
             ..Default::default()
         })
-        .insert(SnakeHead::default())
-        .insert(Position { x: 3, y: 3 })
-        .insert(Size::square(0.8));
+        .insert(SnakeSegment)
+        .insert(position)
+        .insert(Size::square(0.65))
+        .id()
+}
+
+fn spawn_snake(mut commands: Commands, mut segments: ResMut<SnakeSegments>) {
+    *segments = SnakeSegments(vec![
+        commands
+            .spawn(Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..Default::default()
+            })
+            .insert(SnakeHead::default())
+            .insert(Position { x: 3, y: 3 })
+            .insert(Size::square(0.8))
+            .id(),
+        spawn_segment(commands, Position { x: 3, y: 2 }),
+    ]);
 }
 
 fn handle_input(input: Res<ButtonInput<KeyCode>>, mut heads: Query<&mut SnakeHead>) {
@@ -115,18 +150,38 @@ impl MovementTimer {
 fn movement(
     time: Res<Time>,
     mut timer: ResMut<MovementTimer>,
-    mut heads: Query<(&mut Position, &SnakeHead)>,
+    segments: ResMut<SnakeSegments>,
+    mut heads: Query<(Entity, &SnakeHead)>,
+    mut positions: Query<&mut Position>,
 ) {
     if !timer.clock.tick(time.delta()).just_finished() {
         return;
     }
-    if let Some((mut pos, head)) = heads.iter_mut().next() {
-        match &head.direction {
-            Direction::Left => pos.x -= 1,
-            Direction::Up => pos.y += 1,
-            Direction::Right => pos.x += 1,
-            Direction::Down => pos.y -= 1,
+    if let Some((head_entity, head)) = heads.iter_mut().next() {
+        let segment_positions: Vec<Position> = segments
+            .iter()
+            .filter_map(|e| positions.get_mut(*e).ok().map(|p| *p))
+            .collect();
+        if segment_positions.len() != segments.len() {
+            // Some segments were missing positions, exit early
+            return;
         }
+        if let Ok(mut head_pos) = positions.get_mut(head_entity) {
+            match &head.direction {
+                Direction::Left => head_pos.x -= 1,
+                Direction::Up => head_pos.y += 1,
+                Direction::Right => head_pos.x += 1,
+                Direction::Down => head_pos.y -= 1,
+            }
+        }
+        segment_positions
+            .iter()
+            .zip(segments.iter().skip(1))
+            .for_each(|(pos, segment)| {
+                if let Ok(mut position) = positions.get_mut(*segment) {
+                    *position = *pos
+                }
+            });
     }
 }
 
@@ -136,7 +191,8 @@ impl Plugin for SnakePlugin {
     fn build(&self, app: &mut bevy::app::App) {
         let timer = MovementTimer::from_seconds(0.150);
         app.insert_resource(timer);
-        app.add_systems(Startup, spawn);
+        app.insert_resource(SnakeSegments::default());
+        app.add_systems(Startup, spawn_snake);
         app.add_systems(FixedUpdate, (handle_input, movement).chain());
     }
 }
